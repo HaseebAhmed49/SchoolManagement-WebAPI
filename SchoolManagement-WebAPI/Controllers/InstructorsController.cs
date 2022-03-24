da10a1973913274f0db088a0fc915f9a89ac939b
HaseebAhmed49 <haseebahmed02@gmail.com>
2022-03-25 02:45:48 +0500

    Added Exception Handling for Instructor Entity

--- SchoolManagement-WebAPI/Controllers/InstructorsController.cs
+++ SchoolManagement-WebAPI/Controllers/InstructorsController.cs
@@ -24,43 +24,77 @@ namespace SchoolManagement_WebAPI.Controllers
         [HttpPost("add-instructor-with-courses")]
         public IActionResult AddInstructor([FromBody]InstructorVM instructor)
         {
-            _instructorSevice.AddInstructorWithCourses(instructor);
-            return Ok();
+            try
+            {
+                if (String.IsNullOrEmpty(instructor.LastName) || String.IsNullOrEmpty(instructor.FirstMidName)) return BadRequest("First Mid Name or Last Name are not valid");
+                _instructorSevice.AddInstructorWithCourses(instructor);
+                return Ok(instructor);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500,ex);
+            }
         }
 
         [HttpGet("get-all-instructors")]
         public IActionResult GetAllInstructors()
         {
-            var allInstructors = _instructorSevice.GetAllInstructors();
-            return Ok(allInstructors);
+            try
+            {
+                var allInstructors = _instructorSevice.GetAllInstructors();
+                return (allInstructors.Count > 0) ? Ok(allInstructors) : BadRequest("No Instructors Exists");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
         }
-
-        //[HttpGet("get-instructor-by-id/{id}")]
-        //public IActionResult GetInstructorById(int id)
-        //{
-        //    var instructor = _instructorSevice.GetInstructorById(id);
-        //    return Ok(instructor);
-        //}
 
         [HttpGet("get-instructor-with-courses-by-id/{id}")]
         public IActionResult GetInstructorwithCoursesById(int id)
         {
-            var instructorwithCourses = _instructorSevice.GetInstructorById(id);
-            return Ok(instructorwithCourses);
+            try
+            {
+                if (id <= 0) return BadRequest("Id can't be -ve 0r 0");
+                var instructorwithCourses = _instructorSevice.GetInstructorById(id);
+                return (instructorwithCourses != null) ? Ok(instructorwithCourses) : BadRequest($"No Instructor Exists against Id: {id}");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
         }
 
         [HttpPut("update-instructor-by-id/{id}")]
         public IActionResult UpdateInstructorById(int id,[FromBody]InstructorVM instructor)
         {
-            var updatedInstructor = _instructorSevice.UpdateInstructorById(id,instructor);
-            return Ok(updatedInstructor);
+            try
+            {
+                if (id <= 0) return BadRequest("Id can't be -ve 0r 0");
+                if (String.IsNullOrEmpty(instructor.LastName) || String.IsNullOrEmpty(instructor.FirstMidName)) return BadRequest("First Mid Name or Last Name are not valid");
+                var updatedInstructor = _instructorSevice.UpdateInstructorById(id, instructor);
+                return Ok(updatedInstructor);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
         }
 
         [HttpDelete("delete-instructor-by-id/{id}")]
         public IActionResult DeleteInstructorById(int id)
         {
-            _instructorSevice.DeleteInstructorById(id);
-            return Ok();
+            try
+            {
+                if (id <= 0) return BadRequest("Id can't be -ve 0r 0");
+                _instructorSevice.DeleteInstructorById(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
+
         }
     }
 }
